36dee9ad6641d64f177b60fcfc5c7104a13676d7
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-03-07 13:20:54 +0200

    webApp for energy drinks by mahmood alashqar

---
+++ energy.js
@@ -0,0 +1,17 @@
+function energy_drinks(){
+var order = prompt("what the energy drink you want sir, we have : boomboom,redbull,codered ?");
+while (order !='boomboom' && order != 'codered' && order !='redbull')
+{
+order=prompt("please sir choose item just from the menu");
+} 
+var itemOrder=' ';
+if (order=="boomboom"){itemOrder = '<img src="/images/boomboom.jpg"/>' ;}
+else if (order=="codered"){itemOrder = '<img src="/images/codered.jpg"/>' ;}
+else if (order=="redbull"){itemOrder = '<img src="/images/redbull.jpg"/>' ;}
+console.log(itemOrder);
+var numberOfBottle = prompt("how many bottle you want sair ?");
+for (var i=0;i<numberOfBottle;i++)
+{
+    document.write(itemOrder);
+}
+}
