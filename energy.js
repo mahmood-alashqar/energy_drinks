5f9e706f35997afdab2885c7b95fd4825e98a361
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-03-08 12:38:17 +0200

    function for js by mahmood alashqar

--- energy.js
+++ energy.js
@@ -15,3 +15,18 @@ for (var i=0;i<numberOfBottle;i++)
     document.write(itemOrder);
 }
 }
+
+function tempFun()
+{
+ for (var i=0;i<3;i++)
+{
+    document.write('<br>'+"test function"+'<br>');
+}   
+}
+var temp2Fun=function ()
+{
+    document.write("Welcom To The Energy Drink Store!");
+}
+
+tempFun(); 
+temp2Fun();
